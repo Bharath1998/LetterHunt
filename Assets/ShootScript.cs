bf9fb87bb55cf374534a02e5c5c852366ce96ec5
Someshwar-Balaraman <someshwarb1999@gmail.com>
2022-09-13 00:45:59 -0700

    Update ShootScript.cs
    Destroying bullets after 2 seconds.

--- Assets/ShootScript.cs
+++ Assets/ShootScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class ShootScript : MonoBehaviour
 {
     public float LaunchForce;
-
     public GameObject Bullet;
 
     // Start is called before the first frame update
@@ -13,19 +12,19 @@ public class ShootScript : MonoBehaviour
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
             Shoot();
-        }
+        }     
     }
 
     void Shoot()
     {
         GameObject BulletIns = Instantiate(Bullet, transform.position, transform.rotation);
-        BulletIns.GetComponent<Rigidbody2D>().AddForce(transform.right * LaunchForce);
+        BulletIns.GetComponent<Rigidbody2D>().AddForce(transform.right * LaunchForce);   
+        Destroy(BulletIns.gameObject, 2f);
     }
 }
