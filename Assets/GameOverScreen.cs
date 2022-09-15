5ea7d75452e19959603867edb6fc0ddfbcb71356
Bharath1998 <bharath.raghunath@gmail.com>
2022-09-15 15:09:56 -0700

    Game intro (#8)
    * Start Ending Screen
    
    * Fixed Unity Bug

---
+++ Assets/GameOverScreen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public void RestartGame(){
+        // For now level 1 - maybe restart at current level using build index -1
+        Debug.Log("Hi");
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void ExitToMain(){
+        Debug.Log("Byee");
+        SceneManager.LoadScene("Menu");
+    }
+}
