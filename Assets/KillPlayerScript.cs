7c403b88f6a2357a00bc900d561ce08951edec26
Akanksha Nath <akankshanath@Akankshas-MacBook-Pro.local>
2022-09-16 00:13:42 -0700

    Update code for spawning enemies

--- Assets/KillPlayerScript.cs
+++ Assets/KillPlayerScript.cs
@@ -18,7 +18,7 @@ public class KillPlayerScript : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision){
-    	if(collision.gameObject.name == "Enemy"){
+    	if(collision.gameObject.name == "Enemy1"){
     		Destroy(gameObject);
     		Camera cam = Camera.main;
     		GameObject newCam = new GameObject("newMainCam");
