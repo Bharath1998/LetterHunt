0e421c44d03288131f0ae9d7056e9513d38a0cea
Someshwar-Balaraman <someshwarb1999@gmail.com>
2022-09-17 17:25:26 -0700

    Reading JSON to get letters to be displayed
    Adding a JSON reader to LetterSpawner.cs to randomly select a word for a given level and getting the letters we have to display for that particular word.

--- Assets/LetterSpawner.cs
+++ Assets/LetterSpawner.cs
@@ -12,11 +12,57 @@ public class LetterSpawner : MonoBehaviour
     private int randomIndex;
     private float randomX;
     private float randomY;
-    private int[] index = {0, 5, 4, 8, 15, 25, 1, 0, 16};
+    //private int[] index = {0, 5, 4, 8, 15, 25, 1, 0, 16};
+    private int[] index = new int[26];
     private int i = 0;
+
+    private string target_word;
+    private int[] blanks;
+    private string[] letters;
+
+    public TextAsset textJSON;
+
+    [System.Serializable]
+    public class Player
+    {
+        public string target_word;
+        public int[] blanks;
+        public string[] letters;
+    }
+
+    [System.Serializable]
+    public class PlayerList
+    {
+        public Player[] lvl1;
+    }
+
+    public PlayerList myPlayerList = new PlayerList();
     // Start is called before the first frame update
     void Start()
     {
+        myPlayerList = JsonUtility.FromJson<PlayerList>(textJSON.text);
+        int len = myPlayerList.lvl1.Length;
+        int rInt = Random.Range(0, len);
+        int i=0;
+        foreach (Player player in myPlayerList.lvl1)
+        {
+            if(i==rInt){
+                target_word = player.target_word;
+                blanks = player.blanks;
+                letters = player.letters;
+            }
+            i++;
+            
+        }
+
+        for(int j=0; j<letters.Length; j++)
+        {
+            int ascii = (int)letters[j][0];
+            ascii = ascii - 65;
+            index[j] = ascii;
+            //print(index[j]);
+        }
+        print("Word is ="+target_word);
         StartCoroutine(SpawnLetters());
     }
 
