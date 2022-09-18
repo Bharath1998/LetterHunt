829d87530b6b0ebbe1af8a7f344028f0aa17521e
Bharath1998 <bharath.raghunath@gmail.com>
2022-09-17 18:04:57 -0700

    Player camera

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody2D player;
     private Animator anim;
+    private Vector2 movement;
+    bool facingRight = true;
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
@@ -17,27 +19,61 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
-        player.velocity = new Vector2(dirX * 7f, player.velocity.y);
+        // float dirX = Input.GetAxisRaw("Horizontal");
+        // player.velocity = new Vector2(dirX * 7f, player.velocity.y);
 
+        // if(Input.GetKey(KeyCode.W))
+        // {
+        //     player.velocity = new Vector2(player.velocity.x,7);
+        // }
+
+        // bool flipped = dirX <0 ;
+        // transform.rotation = Quaternion.Euler(new Vector3(0f, flipped ? 180f : 0f, 0f));
+
+
+        // if(dirX > 0f){
+        //     anim.SetBool("running",true);
+        // }
+        // else if (dirX  < 0f)
+        // {   
+        //     anim.SetBool("running",true);
+        // }
+        // else{
+        //     anim.SetBool("running",false);
+
+        // }
+
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"),0).normalized ;
+        anim.SetFloat("Speed", Mathf.Abs(movement.magnitude * 3f));
         if(Input.GetKey(KeyCode.W))
         {
-            player.velocity = new Vector2(player.velocity.x,7);
+            float dirX = Input.GetAxisRaw("Horizontal");
+            // player.velocity = new Vector2(dirX * 6f, player.velocity.y);
+            player.velocity = new Vector2(player.velocity.x,6);
         }
-        if(dirX > 0f){
-            anim.SetBool("running",true);
-        }
-        else if (dirX < 0f)
-        {   
-            anim.SetBool("running",true);
-        }
-        else{
-            anim.SetBool("running",false);
+        bool flipped = movement.x <0 ;
 
-        }
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, flipped ? 180f : 0f, 0f));
+
 
 
     }
+
+    private void FixedUpdate(){
+        if (movement != Vector2.zero){
+            var xMovement = movement.x * 3f * Time.deltaTime;
+            this.transform.Translate(new Vector3(xMovement,0), Space.World);
+        }
+
+    }
+
+    void Flip(){
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
+    }
+
 
     void OnCollisionEnter2D(Collision2D other)
     {
