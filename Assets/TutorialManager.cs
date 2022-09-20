c2dbcd918fd7f8c1200cc113c7bf66a9a0e51f27
Bharath1998 <bharath.raghunath@gmail.com>
2022-09-19 23:19:23 -0700

    In Game tutorial - need fixes

---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex=0;
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        for (int i=0;i<popUps.Length;i++){
+            if(i==popUpIndex){
+                popUps[popUpIndex].SetActive(true);
+            }
+            else{
+                popUps[popUpIndex].SetActive(false);
+            }
+        }
+        
+        if(popUpIndex == 0){
+            if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)){
+                popUpIndex++;
+            }
+        }
+        else if(popUpIndex == 1)
+        {
+            if(Input.GetKeyDown(KeyCode.UpArrow)){
+                    popUpIndex++;
+                }
+        }
+
+        if (popUpIndex==2){
+            enabled = false;
+        }
+        
+            // else if (popUpIndex ==2){
+            //     // Spawn Enemy and show player shooting.
+            // }
+    }
+}
+
