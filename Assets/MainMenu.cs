7f8ea8abff63116a6fd60c38fd3634c00afecd74
Bharath Raghunath <bharath.raghunath@gmail.com>
2022-09-27 00:35:58 -0700

    Power Up rough tutorial (#35)

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
 
     public void PlayGame(){
-        SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene("Level0");
     }
 
     public void Tutorials(){
