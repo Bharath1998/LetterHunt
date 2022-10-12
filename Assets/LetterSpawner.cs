ad30105c845b9f4942250fdd517ef6c685865681
Someshwar-Balaraman <someshwarb1999@gmail.com>
2022-10-11 17:28:15 -0700

    created 3 different buckets of letters. edited JSON to new format

--- Assets/LetterSpawner.cs
+++ Assets/LetterSpawner.cs
@@ -15,14 +15,18 @@ public class LetterSpawner : MonoBehaviour
     private float randomX;
     private float randomY;
     //private int[] index = {0, 5, 4, 8, 15, 25, 1, 0, 16};
-    private int[] index;
+    private int[] green_index;
+    private int[] orange_index;
+    private int[] red_index;
     private int[] target_index;
     private int i = 0;
     public List<float[]> seenList;
 
     public static string target_word;
     private int[] blanks;
-    private string[] letters;
+    private string[] green_letters;
+    private string[] orange_letters;
+    private string[] red_letters;
 
     public Collider2D[] colliders;
     public float radius;
@@ -34,7 +38,9 @@ public class LetterSpawner : MonoBehaviour
     {
         public string target_word;
         public int[] blanks;
-        public string[] letters;
+        public string[] green_letters;
+        public string[] orange_letters;
+        public string[] red_letters;
     }
 
     [System.Serializable]
@@ -58,18 +64,38 @@ public class LetterSpawner : MonoBehaviour
             if(i==rInt){
                 target_word = player.target_word;
                 blanks = player.blanks;
-                letters = player.letters;
+                green_letters = player.green_letters;
+                orange_letters = player.orange_letters;
+                red_letters = player.red_letters;
             }
             i++;
             
         }
-        index = new int[letters.Length];
+        green_index = new int[green_letters.Length];
+        orange_index = new int[orange_letters.Length];
+        red_index = new int[red_letters.Length];
         target_index = new int[target_word.Length];
-        for(int j=0; j<letters.Length; j++)
+        for(int j=0; j<green_letters.Length; j++)
         {
-            int ascii = (int)letters[j][0];
+            int ascii = (int)green_letters[j][0];
             ascii = ascii - 65;
-            index[j] = ascii;
+            green_index[j] = ascii;
+            //print(index[j]);
+        }
+
+        for(int j=0; j<orange_index.Length; j++)
+        {
+            int ascii = (int)orange_letters[j][0];
+            ascii = ascii - 65;
+            orange_index[j] = ascii;
+            //print(index[j]);
+        }
+
+        for(int j=0; j<red_index.Length; j++)
+        {
+            int ascii = (int)red_letters[j][0];
+            ascii = ascii - 65;
+            red_index[j] = ascii;
             //print(index[j]);
         }
 
