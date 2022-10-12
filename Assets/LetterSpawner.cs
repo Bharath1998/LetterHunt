520497765bd4c599df08587dca765108174d6aa9
Someshwar-Balaraman <someshwarb1999@gmail.com>
2022-10-11 17:55:45 -0700

    while logic for displaying letters from buckets

--- Assets/LetterSpawner.cs
+++ Assets/LetterSpawner.cs
@@ -111,7 +111,7 @@ public class LetterSpawner : MonoBehaviour
 
     IEnumerator SpawnLetters() {
         int t=10;
-        while(i < index.Length){
+        while(i < orange_index.Length && i<yellow_index.Length && i<purple_index.Length){
             if(i!=0){
                 yield return new WaitForSeconds(2);
             } 
@@ -148,16 +148,171 @@ public class LetterSpawner : MonoBehaviour
             //     GameObject power_up_highlight_ins = Instantiate(power_up_highlight);
             //     power_up_highlight_ins.transform.position = randomPosition;
             // }
-            spawnedLetter = Instantiate(letterReference[index[i]]);
-            
+            spawnedLetter = Instantiate(letterReference[orange_index[i]]);           
             spawnedLetter.transform.position = randomPosition;  
-            if(!target_index.Contains(index[i])){
-                //print(index[i]);
-                Destroy(spawnedLetter.gameObject, t);
-                t+=10;
-            } 
+
+            spawnedLetter = Instantiate(letterReference[yellow_index[i]]);           
+            spawnedLetter.transform.position = randomPosition; 
+
+            spawnedLetter = Instantiate(letterReference[purple_index[i]]);           
+            spawnedLetter.transform.position = randomPosition; 
+            // if(!target_index.Contains(index[i])){
+            //     //print(index[i]);
+            //     Destroy(spawnedLetter.gameObject, t);
+            //     t+=10;
+            // } 
             i += 1;      
         }
+
+        int o = i;
+        int y = i;
+        int p = i;
+        i=o;
+        while(i < orange_index.Length){
+            if(i!=0){
+                yield return new WaitForSeconds(2);
+            } 
+            randomIndex = Random.Range(0, letterReference.Length);
+            
+            float x = Random.Range(-7,30);
+            float y = Random.Range(0,16);
+            Vector3 randomPosition = new Vector3(x,y,0);
+            
+            while(true){
+                
+                if( checkCollision(x,y) && ((x >= -8 && x <= -4 && y >=0 && y<=4) ||
+                (x >= -8 && x <= -0.1 && y >=7 && y<=11) ||
+                (x >= -8 && x <= 31 && y >=15.7 && y<=17) ||
+                (x >= 1.75 && x <= 11 && y >=0 && y<=4) ||
+                (x >= 10 && x <= 20 && y >= 2.9 && y<=9) ||
+                (x >= 20 && x <= 31 && y >= 1.75 && y<=4))){
+                    randomPosition = new Vector3(x,y,0);
+                    seenList.Add(new float[2]{x,y});
+                    break;
+
+                }else{
+                    x = Random.Range(-7,30);
+                    y = Random.Range(0,16);
+                }
+
+            }
+
+            if(i==0){
+                randomPosition = new Vector3((float)-5.5,(float)-0.25,0);
+                seenList.Add(new float[2]{-5.5f,-0.25f});
+            }
+            // if(i==2){
+            //     GameObject power_up_highlight_ins = Instantiate(power_up_highlight);
+            //     power_up_highlight_ins.transform.position = randomPosition;
+            // }
+            spawnedLetter = Instantiate(letterReference[orange_index[i]]);
+            
+            spawnedLetter.transform.position = randomPosition;  
+            // if(!target_index.Contains(index[i])){
+            //     //print(index[i]);
+            //     Destroy(spawnedLetter.gameObject, t);
+            //     t+=10;
+            // } 
+            i += 1;      
+        }
+
+        i=y;
+        while(i<yellow_index.Length){
+            if(i!=0){
+                yield return new WaitForSeconds(2);
+            } 
+            randomIndex = Random.Range(0, letterReference.Length);
+            
+            float x = Random.Range(-7,30);
+            float y = Random.Range(0,16);
+            Vector3 randomPosition = new Vector3(x,y,0);
+            
+            while(true){
+                
+                if( checkCollision(x,y) && ((x >= -8 && x <= -4 && y >=0 && y<=4) ||
+                (x >= -8 && x <= -0.1 && y >=7 && y<=11) ||
+                (x >= -8 && x <= 31 && y >=15.7 && y<=17) ||
+                (x >= 1.75 && x <= 11 && y >=0 && y<=4) ||
+                (x >= 10 && x <= 20 && y >= 2.9 && y<=9) ||
+                (x >= 20 && x <= 31 && y >= 1.75 && y<=4))){
+                    randomPosition = new Vector3(x,y,0);
+                    seenList.Add(new float[2]{x,y});
+                    break;
+
+                }else{
+                    x = Random.Range(-7,30);
+                    y = Random.Range(0,16);
+                }
+
+            }
+
+            if(i==0){
+                randomPosition = new Vector3((float)-5.5,(float)-0.25,0);
+                seenList.Add(new float[2]{-5.5f,-0.25f});
+            }
+            // if(i==2){
+            //     GameObject power_up_highlight_ins = Instantiate(power_up_highlight);
+            //     power_up_highlight_ins.transform.position = randomPosition;
+            // }
+            spawnedLetter = Instantiate(letterReference[yellow_index[i]]);
+            
+            spawnedLetter.transform.position = randomPosition;  
+            // if(!target_index.Contains(index[i])){
+            //     //print(index[i]);
+            //     Destroy(spawnedLetter.gameObject, t);
+            //     t+=10;
+            // } 
+            i += 1;      
+        }
+        i=p;
+        while(i<purple_index.Length){
+            if(i!=0){
+                yield return new WaitForSeconds(2);
+            } 
+            randomIndex = Random.Range(0, letterReference.Length);
+            
+            float x = Random.Range(-7,30);
+            float y = Random.Range(0,16);
+            Vector3 randomPosition = new Vector3(x,y,0);
+            
+            while(true){
+                
+                if( checkCollision(x,y) && ((x >= -8 && x <= -4 && y >=0 && y<=4) ||
+                (x >= -8 && x <= -0.1 && y >=7 && y<=11) ||
+                (x >= -8 && x <= 31 && y >=15.7 && y<=17) ||
+                (x >= 1.75 && x <= 11 && y >=0 && y<=4) ||
+                (x >= 10 && x <= 20 && y >= 2.9 && y<=9) ||
+                (x >= 20 && x <= 31 && y >= 1.75 && y<=4))){
+                    randomPosition = new Vector3(x,y,0);
+                    seenList.Add(new float[2]{x,y});
+                    break;
+
+                }else{
+                    x = Random.Range(-7,30);
+                    y = Random.Range(0,16);
+                }
+
+            }
+
+            if(i==0){
+                randomPosition = new Vector3((float)-5.5,(float)-0.25,0);
+                seenList.Add(new float[2]{-5.5f,-0.25f});
+            }
+            // if(i==2){
+            //     GameObject power_up_highlight_ins = Instantiate(power_up_highlight);
+            //     power_up_highlight_ins.transform.position = randomPosition;
+            // }
+            spawnedLetter = Instantiate(letterReference[purple_index[i]]);
+            
+            spawnedLetter.transform.position = randomPosition;  
+            // if(!target_index.Contains(index[i])){
+            //     //print(index[i]);
+            //     Destroy(spawnedLetter.gameObject, t);
+            //     t+=10;
+            // } 
+            i += 1;      
+        }
+
     }
 
     bool checkCollision(float x, float y){
