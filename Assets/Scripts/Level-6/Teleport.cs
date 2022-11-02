e50182e3e5910c98c4bab31d5019409887b69008
Bharath1998 <bharath.raghunath@gmail.com>
2022-11-01 22:56:45 -0700

    Teleport basic

---
+++ Assets/Scripts/Level-6/Teleport.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    public GameObject portal;
+    public GameObject portal2;
+    private GameObject player;
+    private bool door1=true;
+    private bool door1_final=false;
+
+     private void Start() {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.tag=="Player"){
+            if(this.tag=="Door1"){
+                player.transform.position = new Vector2(portal2.transform.position.x-2, portal2.transform.position.y);
+            }
+            else if(this.tag=="Door2"){
+                print("Hiiiii");
+            player.transform.position = new Vector2(portal.transform.position.x-2, portal.transform.position.y);
+
+            }
+
+        }
+    }
+
+
+}
