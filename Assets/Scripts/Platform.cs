0507ffc2f6719874190b8c60218ddbbeb415dde9
Rishikesh Kadam <rishikesh1419@gmail.com>
2022-11-07 21:11:57 -0800

    Level 3 tilemap (#74)
    * Level-5 tilemap
    
    * Added level 3
    
    * new level with moving platforms added
    
    * Level-5 tilemap
    
    * Added level 3
    
    * new level with moving platforms added
    
    * Fix dashes and enemy collision
    
    * Limit enemies as per level
    
    * Change tilemap
    
    Co-authored-by: urjitdesai <urjitdesai1998@gmail.com>
    Co-authored-by: urjithem <97576621+urjithem@users.noreply.github.com>
    Co-authored-by: akanksha0031 <asnath@usc.edu>
    Co-authored-by: Bharath Raghunath <bharath.raghunath@gmail.com>

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using static DataCollection;
 public class Platform : MonoBehaviour
 {
     public Transform pos1, pos2;
     public float speed;
+    public bool canMove;
     public Transform startPos;
-
+    public GameObject player;
     Vector3 nextPos;
     // Start is called before the first frame update
     void Start()
@@ -20,19 +23,35 @@ public class Platform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position == pos1.position){
+        if(canMove){
+            if(transform.position == pos1.position){
             nextPos = pos2.position;
-        }
-        if(transform.position == pos2.position){
-            nextPos = pos1.position;
-        }
-        // Debug.Log(nextPos);
-        // Debug.Lo
+            }
+            if(transform.position == pos2.position){
+                nextPos = pos1.position;
+            }
+            // Debug.Log(nextPos);
+            // Debug.Lo
 
-        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+        }
+        
         
     }
     private void onDrawGizmos(){
         Gizmos.DrawLine(pos1.position, pos2.position);
     }
+
+    private void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.name == "Player"){
+            canMove=true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other){
+        if(other.gameObject.name == "Player"){
+            canMove=false;
+        }
+    }
+
 }
