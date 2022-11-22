07713e4a9587abb18bb884b009150faf15a1bfa7
aaryatad <97655778+aaryatad@users.noreply.github.com>
2022-11-22 15:23:00 -0800

    Add dynamic hint images

--- Assets/hintScript.cs
+++ Assets/hintScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class hintScript : MonoBehaviour
 {
@@ -8,16 +9,30 @@ public class hintScript : MonoBehaviour
     // private GameObject image;
     public static float timer;
     public GameObject button;
-    public GameObject image;
+    // public GameObject image;
     public static string target_word;
     // [SerializeField] private UnityEngine.UI.Image image = null;
+
+    public Canvas imageCanvas;
+
     void Start()
     {
         timer=Timer.currentTime;
         
         target_word = "MAN";
-        image.SetActive(false);
+        // image.SetActive(false);
         button.SetActive(false);
+
+        //find canvas
+        GameObject canvasObject = GameObject.Find("ImageCanvas");
+        if(canvasObject != null){
+            print("CANVAS OBJECT IS NOT NULL");
+            //If we found the object , get the Canvas component from it.
+            imageCanvas = canvasObject.GetComponent<Canvas>();
+            if(imageCanvas == null){
+                Debug.Log("Could not locate Canvas component on " + canvasObject.name);
+            }
+        }
     }
 
     
@@ -38,16 +53,32 @@ public class hintScript : MonoBehaviour
     {
         print("Button CLICKED");
         // image.sprite = Resources.Load<Sprite>( "HintImages/" + target_word+".jpg" );
-        image = Resources.Load("HintImages/" + target_word+".jpg") as GameObject;
-        // if (image != null)
-        {
-            print("HINT IMAGE ============= working");
+        //image = Resources.Load("HintImages/" + target_word+".jpg") as GameObject;
+        
+        GameObject newObject = new GameObject("hintImage");
+        newObject.transform.SetParent(imageCanvas.transform);
+        newObject.AddComponent<Image>();
+        newObject.GetComponent<Image>().sprite = Resources.Load<Sprite>( "HintImages/" + target_word+".jpg" );
+        print("HintImages/" + target_word+".jpg");
+        if(Resources.Load("HintImages/" + target_word+".jpg", typeof(Sprite)) as Sprite != null){
+            print("IMAGE IS NOT NULL");
+
         }
-        GameObject ins_image = Instantiate(image);
-        ins_image.SetActive(true);
-        // yield return new WaitForSeconds(3);
-        // image.SetActive(false);
-        Destroy(ins_image,3f);
+        else{
+            print("IMAGE IS NULL");
+        }
+
+
+        // if (image != null)
+        // {
+        //     print("HINT IMAGE ============= working");
+        // }
+        // GameObject ins_image = Instantiate(image);
+
+        // ins_image.SetActive(true);
+        // // yield return new WaitForSeconds(3);
+        // // image.SetActive(false);
+        // Destroy(ins_image,3f);
 
     }
 }
