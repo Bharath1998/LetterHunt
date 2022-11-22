c1daf6ddde2a0d34c4eee32f384411bbc9ad8f3d
aaryatad <97655778+aaryatad@users.noreply.github.com>
2022-11-22 00:35:13 -0800

    Show image on click

--- Assets/hintScript.cs
+++ Assets/hintScript.cs
@@ -5,14 +5,41 @@ using UnityEngine;
 public class hintScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    // private GameObject image;
+    public static float timer;
+    public GameObject button;
+    public GameObject image;
     void Start()
     {
+        timer=Timer.currentTime;
+        
+        
+        image.SetActive(false);
+        button.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+
+        timer=(int)Timer.currentTime;
+        print("FROM HINT:"+timer);
+        if(timer == 30){
+            button.SetActive (true);
+            
+            return;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void hintOnClick()
     {
-        
+        print("Button CLICKED");
+        image = Resources.Load("a/red_a_b_" + char.ToLower(lastCharacter)) as GameObject;
+        image.SetActive(true);
+        yield return new WaitForSeconds(3);
+        image.SetActive(false);
+        //Destroy(image,3f);
+
     }
 }
