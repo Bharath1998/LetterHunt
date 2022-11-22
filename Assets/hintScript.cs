001b22ccf9da5feddbe7d966458944ecad5c65e2
aaryatad <97655778+aaryatad@users.noreply.github.com>
2022-11-22 01:01:30 -0800

    Generalizing images in progress

--- Assets/hintScript.cs
+++ Assets/hintScript.cs
@@ -9,11 +9,13 @@ public class hintScript : MonoBehaviour
     public static float timer;
     public GameObject button;
     public GameObject image;
+    public static string target_word;
+    // [SerializeField] private UnityEngine.UI.Image image = null;
     void Start()
     {
         timer=Timer.currentTime;
         
-        
+        target_word = "MAN";
         image.SetActive(false);
         button.SetActive(false);
     }
@@ -23,7 +25,7 @@ public class hintScript : MonoBehaviour
     {
 
         timer=(int)Timer.currentTime;
-        print("FROM HINT:"+timer);
+        // print("FROM HINT:"+timer);
         if(timer == 30){
             button.SetActive (true);
             
@@ -35,11 +37,17 @@ public class hintScript : MonoBehaviour
     public void hintOnClick()
     {
         print("Button CLICKED");
-        image = Resources.Load("a/red_a_b_" + char.ToLower(lastCharacter)) as GameObject;
-        image.SetActive(true);
-        yield return new WaitForSeconds(3);
-        image.SetActive(false);
-        //Destroy(image,3f);
+        // image.sprite = Resources.Load<Sprite>( "HintImages/" + target_word+".jpg" );
+        image = Resources.Load("HintImages/" + target_word+".jpg") as GameObject;
+        // if (image != null)
+        {
+            print("HINT IMAGE ============= working");
+        }
+        GameObject ins_image = Instantiate(image);
+        ins_image.SetActive(true);
+        // yield return new WaitForSeconds(3);
+        // image.SetActive(false);
+        Destroy(ins_image,3f);
 
     }
 }
